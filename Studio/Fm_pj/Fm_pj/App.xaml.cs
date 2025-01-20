ee227c9293b5cf1e6f5df0e50e8c22a8f364abb1
Roberto.Morlacchi@wolterskluwer.com <Roberto.Morlacchi@wolterskluwer.com>
2025-01-20 15:23:23 +0100

    feat(Studio): Aggiunto la solution per iniziare a creare i primi oggetti

---
+++ Studio/Fm_pj/Fm_pj/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Fm_pj
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
